ef1fadf62f032c7b537989fbf7dfd9dabb853444
Pierre Bretéché <pbreteche@dawan.fr>
2025-08-26 14:01:39 +0200

    utilisation du cache contract

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,14 +9,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 #[Route('/')]
 class DefaultController extends AbstractController
 {
     #[Route('')]
     #[Cache(expires: 'tomorrow midnight', maxage: 3600, public: true)]
-    public function index(Request $request): Response
-    {
+    public function index(
+        Request $request,
+        CacheInterface $cache,
+    ): Response {
         $response = new Response();
         // L'etag est une chaîne de caractères qui doit unique pour chaque version du document
         // On reste complètement libre de le calculer comme on veut.
@@ -27,6 +31,20 @@ class DefaultController extends AbstractController
             return $response;
         }
 
-        return $this->render('default/index.html.twig');
+        // Ici, il n'y a pas d'anticipation afin d'écrire la donnée en prévision de pouvoir la lire ultérieurement.
+        // On tente seulement de lire la donnée, et on fournit une fonction de régénération au cas où la donnée
+        // n'existe pas, ou elle existe, mais est périmée.
+        $cachedData = $cache->get('notre_cle_unique', function (ItemInterface $item) {
+            $item->expiresAfter(3600);
+
+            // Calcul couteux
+            usleep(500000);
+
+            return 'la donnée produite par un calcul couteux';
+        });
+
+        return $this->render('default/index.html.twig', [
+            'data' => $cachedData,
+        ]);
     }
 }

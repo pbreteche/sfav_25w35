878ccf7bcf9cc38c58be195606437555a0e85bd0
Pierre Bretéché <pbreteche@dawan.fr>
2025-08-26 13:29:10 +0200

    HTTP Cache : modèle de validation

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,8 +15,18 @@ class DefaultController extends AbstractController
 {
     #[Route('')]
     #[Cache(expires: 'tomorrow midnight', maxage: 3600, public: true)]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $response = new Response();
+        // L'etag est une chaîne de caractères qui doit unique pour chaque version du document
+        // On reste complètement libre de le calculer comme on veut.
+        // On choisira une méthode à faible coût (md5, autre).
+        $response->setEtag($request->getLocale());
+        if ($response->isNotModified($request)) {
+            // réponse sans corps avec statut "304 Not Modified"
+            return $response;
+        }
+
         return $this->render('default/index.html.twig');
     }
 }

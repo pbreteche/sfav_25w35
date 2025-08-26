d6c7270922751b06f29a73fdd8877051e67b3031
Pierre Bretéché <pbreteche@dawan.fr>
2025-08-26 11:25:10 +0200

    Informations de cache via les attributs

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,26 +6,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/')]
 class DefaultController extends AbstractController
 {
     #[Route('')]
+    #[Cache(expires: 'tomorrow midnight', maxage: 3600, public: true)]
     public function index(): Response
     {
-        $response = $this->render('default/index.html.twig');
-
-        // La gestion du cache HTTP se fait directement sur la réponse
-        // via les en-têtes "cache-control", "expires", "etag", etc.
-        // En environnement de dev, Symfony force à avoir un rafraîchissement systématique
-        $response->headers->set('cache-control', 'max-age=0, public');
-        // Il existe des raccourcis pour manipuler spécifiquement ces en-têtes
-        $response->headers->addCacheControlDirective('no-cache');
-        $response->headers->addCacheControlDirective('public');
-        $response->setPublic();
-        $response->setMaxAge(3600);
-
-        return $response;
+        return $this->render('default/index.html.twig');
     }
 }

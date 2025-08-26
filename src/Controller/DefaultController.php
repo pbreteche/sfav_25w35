c187f9dac05302a338df7c9efd3c256eef5646d9
Pierre Bretéché <pbreteche@dawan.fr>
2025-08-26 11:21:05 +0200

    Gérer le cache HTTP sur l'objet Response

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,6 +14,18 @@ class DefaultController extends AbstractController
     #[Route('')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig');
+        $response = $this->render('default/index.html.twig');
+
+        // La gestion du cache HTTP se fait directement sur la réponse
+        // via les en-têtes "cache-control", "expires", "etag", etc.
+        // En environnement de dev, Symfony force à avoir un rafraîchissement systématique
+        $response->headers->set('cache-control', 'max-age=0, public');
+        // Il existe des raccourcis pour manipuler spécifiquement ces en-têtes
+        $response->headers->addCacheControlDirective('no-cache');
+        $response->headers->addCacheControlDirective('public');
+        $response->setPublic();
+        $response->setMaxAge(3600);
+
+        return $response;
     }
 }

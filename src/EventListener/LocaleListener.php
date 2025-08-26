9a2de9ee7f1c45f73b5c62f3457de25a5917d613
Pierre Bretéché <pbreteche@dawan.fr>
2025-08-26 09:33:20 +0200

    création d'un écouteur pour lire la session

--- src/EventListener/LocaleListener.php
+++ src/EventListener/LocaleListener.php
@@ -2,7 +2,6 @@
 
 namespace App\EventListener;
 
-use App\Constants;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 
@@ -12,20 +11,34 @@ use Symfony\Component\HttpKernel\Event\RequestEvent;
  *
  * → Notre priorité doit donc être strictement supérieur à 16
  */
-#[AsEventListener(priority: 32)]
-final readonly class AcceptLanguageListener
+#[AsEventListener(method: 'setFromAcceptLanguage', priority: 32)]
+#[AsEventListener(method: 'setFromSession', priority: 31)]
+final readonly class LocaleListener
 {
     public function __construct(private array $managedLocales)
     {
     }
 
-    public function __invoke(RequestEvent $event): void
+    public function setFromAcceptLanguage(RequestEvent $event): void
     {
         $request = $event->getRequest();
         $preferredLocale = $request->getPreferredLanguage($this->managedLocales);
 
         if ($preferredLocale) {
             $request->setLocale($preferredLocale);
+        }
+    }
+
+    /*
+     * Ici, il serait aussi simple et plus efficace d'avoir les deux traitements dans une seule méthode.
+     */
+    public function setFromSession(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+        $sessionLocale = $request->getSession()->get('locale');
+
+        if ($sessionLocale) {
+            $request->setLocale($sessionLocale);
         }
     }
 }

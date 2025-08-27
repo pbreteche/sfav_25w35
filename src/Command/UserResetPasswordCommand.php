b54ba99c48377ecd85c7487eaa4a0080550a7535
Pierre Bretéché <pbreteche@dawan.fr>
2025-08-27 11:58:14 +0200

    création de la commande

---
+++ src/Command/UserResetPasswordCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:user:reset-password',
+    description: 'Reset user password',
+)]
+class UserResetPasswordCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $output->writeln('Bonjour, la commande s\'exécute.');
+
+        $name = $io->ask('Quel est votre nom ?');
+
+        $io->title('bonjour '.$name);
+        $io->info('bonjour '.$name);
+        $io->success('bonjour '.$name);
+        $io->warning('bonjour '.$name);
+
+        return Command::SUCCESS;
+    }
+}

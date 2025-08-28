d6c6390e0880ada4f6f669f0303447555785b4e3
Pierre Bretéché <pbreteche@dawan.fr>
2025-08-28 16:20:59 +0200

    passage de paramètre

--- src/Twig/Components/LocaleSelector.php
+++ src/Twig/Components/LocaleSelector.php
@@ -7,6 +7,8 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent]
 final class LocaleSelector
 {
+    public string $buttonClass = 'btn-primary';
+
     public function __construct(
         public array $managedLocales,
     ) {
